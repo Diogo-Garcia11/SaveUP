b381802572d0ae8adc050be1a3465d7d00c288dc
Vinicius2709 <vinitargino2705@gmail.com>
2026-05-16 19:35:59 -0300

    card de roupas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,90 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <style>
+            div.cartao-perfil {
+            width: 300px;
+            max-width: 100%;
+            background-color: white;
+            box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            padding: 20px;
+            margin: 40px auto;
+            transition: 0.3s;
+        }
+
+        div.cartao-perfil:hover {
+            box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.2);
+        }
+
+        div.cartao-perfil img {
+            border-radius: 50%;
+            width: 150px;
+            display: block;
+            margin: 0 auto 15px;
+            object-fit: cover;
+        }
+
+        h1 {
+            color: #2d3748;
+            text-align: center;
+            font-size: 24px;
+        }
+
+        h2 {
+            color: #4a5568;
+            text-align: center;
+            font-size: 18px;
+        }
+
+        p {
+            color: #718096;
+            text-align: center;
+        }
+
+        p.bio {
+            color: #4a5568;
+            text-align: justify;
+            margin-top: 10px;
+        }
+
+        .contato {
+            display: flex;
+            justify-content: center;
+            gap: 30px;
+            margin-top: 15px;
+        }
+
+        .contato a {
+            display: flex;
+            flex-direction: column; 
+            align-items: center;    
+            color: #4299e1;
+            text-decoration: none;
+        }
+
+        .contato a:visited {
+            color: #2b6cb0;
+        }
+
+        .contato a:hover {
+            color: #3182ce;
+        }
+
+        .contato i {
+            font-size: 24px;
+            margin-bottom: 5px;
+        }
+
+        .produtos-grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 20px;
+        }
+
+        
+        </style>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
