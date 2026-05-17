2af0e43018488ebf927d3faa96669f3bbb497b7c
Diogo Pinheiro Garcia <diogogarcia08@gmail.com>
2026-05-17 02:18:59 -0300

    Add Produto CRUD and menu item
    Add a Backpack CRUD for Produto: new ProdutoCrudController and ProdutoRequest, register the route (Route::crud('produto', ...)), and add a 'Produtos' menu item in the admin menu. Update the Produto model to use Backpack's CrudTrait and reference the 'produto' table. The controller scaffolds List/Create/Update/Delete/Show operations and validation wiring using ProdutoRequest.

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
+    use CrudTrait;
      protected $table = 'produto';
 
     // Se a chave primária não for 'id', defina-a aqui
